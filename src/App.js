edf36b3d3c9b95be038dc92478ed350a62a79d03
silikhe <silaswans@gmail.com>
2022-07-18 22:39:18 +0300

    add boilerplate code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+     <h1>Hi, my name is 01</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ["SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", "monospace"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
