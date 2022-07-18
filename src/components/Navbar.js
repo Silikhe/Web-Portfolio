4228ac7ba2b8e8606bc4bad044cf2dc7f826ffb7
Silikhe <silaswans@gmail.com>
2022-07-19 01:27:26 +0300

    finnish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { BeakerIcon } from '@heroicons/react/solid'
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 export default function Navbar() {
   return (
-    <div className='w-screen h-[80px] z-10 bg-zinc-200 drop-shadow-sm '>navbar
-    
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-sm '>
+      <div className='px-2 flex justify-between items-center w-full h-full'>
+        <div className='flex justify-between items-center'>
+          <h1 className='font-2xl font-bold  ml-10 sm:text-3xl'>Silikhe.</h1>
 
+        </div>
+        <div className='hidden md:flex pr-10'>
+          <ul className='hidden md:flex pr-5  '>
+            <li>01. About</li>
+            <li>01. Experience</li>
+            <li>01. Work </li>
+            <li>01. Contract</li>
+          </ul>
+          <div className='flex justify-between items-center'>
+            <button className=''>Resume</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
