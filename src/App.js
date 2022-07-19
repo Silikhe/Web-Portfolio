06aaf597dd47e66791806075d1faff63946dcc7b
Silikhe <silaswans@gmail.com>
2022-07-19 23:04:18 +0300

    navbar design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className='bg-[#0A192F]'>
       <Navbar />
+      <Body/>
     </div>
   );
 }
