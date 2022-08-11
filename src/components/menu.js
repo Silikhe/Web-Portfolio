4222851a562316cbc9fc5bcb3b831638f96610e8
Silas Silikhe <65366942+Silikhe@users.noreply.github.com>
2022-08-11 14:21:21 +0300

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume1.pdf" className="resume-link">
+            <a href="/silikhe_resume.1.2.pdf" className="resume-link">
               Resume
             </a>
           </nav>
